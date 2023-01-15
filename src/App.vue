d5ed817c2569de4a2c8f9e29c06f7f8e9e3f3958
pavel <pavel-sheremet-dev@yandex.ru>
2023-01-15 23:30:16 +0300

    dummy init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <v-navigation-drawer v-model="showNavigation">Navigation</v-navigation-drawer>
+
+        <v-app-bar>
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon @click.stop="showNavigation = !showNavigation"></v-app-bar-nav-icon>
+            </template>
+
+            <v-app-bar-title>Application</v-app-bar-title>
+        </v-app-bar>
+
+        <v-main>content</v-main>
+    </v-app>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const showNavigation = ref(false)
+
+</script>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+    .use(router)
+    .use(vuetify)
+
+app.mount('#app')
